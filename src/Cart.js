4c6329f37d8bf188a02392bce42e7cfc9c8c8f82
SachinJacob <sachinsureshjacob@karunya.edu.in>
2022-06-28 13:04:40 +0530

    fixed cart bugs

--- src/Cart.js
+++ src/Cart.js
@@ -1,49 +1,48 @@
 import React from 'react'
-import {useGlobal} from "./context-api"
+import { useGlobal } from "./context-api"
 import Cartitem from './Cartitem';
 function Cart() {
-const value = useGlobal()
+  const value = useGlobal()
 
-if(value.addtocart.length === 0){
-  return(
-    <div>
-        <br/>
+  if (value.addtocart.length === 0) {
+    return (
+      <div>
+        <br />
         <div className='d-flex justify-content-center'><h1>YOUR BAG</h1></div>
         <div className='d-flex justify-content-center'><h2>Your cart is empty!</h2></div>
       </div>
-  )
-}
+    )
+  }
   return (
     <div>
-      <div id='card-text'><br/>
-      <div className='d-flex justify-content-center'><h1>YOUR BAG</h1></div>
-      <div className='container' id='cart'><br/>
-      {
-      value.addtocart.map((item) => {
-        return ( 
-          <Cartitem key={item} item={item}>
-            
-          </Cartitem>
-        )
-      })}
-        
-        <hr/>
-        <div className='row d-flex justify-content-evenly'>
-          <div className='col-4 d-flex justify-content-center'>
-            <h2>TOTAL : </h2>
+      <div id='card-text'><br />
+        <div className='d-flex justify-content-center'><h1>YOUR BAG</h1></div>
+        <div className='container' id='cart'><br />
+          <div className='row'>
+            {
+              value.addtocart.map((item) => {
+                return (
+                  <Cartitem key={item} item={item} />
+                )
+              })}
+            <hr />
           </div>
-          <div className='col-5 d-flex justify-content-center'>
-            <h2>${}</h2>
+          <div className='row d-flex justify-content-evenly'>
+            <div className='col-4 d-flex justify-content-center'>
+              <h2>TOTAL : </h2>
+            </div>
+            <div className='col-5 d-flex justify-content-center'>
+              <h2>${ }</h2>
+            </div>
           </div>
+        </div><br />
+        <div className='d-flex justify-content-center'>
+          <button type="button" className="btn btn-outline-danger" >CLEAR BAG</button>
         </div>
-      </div><br/>
-      <div className='d-flex justify-content-center'>
-      <button type="button" className="btn btn-outline-danger" >CLEAR BAG</button>
+        <br />
       </div>
-      <br/>
+
     </div>
-      
-      </div>
   )
 }
 
--- src/Cartitem.js
+++ src/Cartitem.js
@@ -1,68 +1,73 @@
-import React,{useState,useEffect,useCallback} from 'react'
-import {IoIosArrowUp,IoIosArrowDown} from "react-icons/io"
-
-const Cartitem = ({item}) => {
-    console.log(item)
+import React, { useState, useEffect, useCallback } from 'react'
+import Loading from './loading'
+import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io"
+const Cartitem = ({ item }) => {
     const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="
-    const [cocktail, setCocktail] = useState(null);
+    const [cocktailnew, setCocktailnew] = useState(null);
+    const [loading, setLoading] = useState(true);
     const fetchData = useCallback(async () => {
         try {
-          const response = await fetch(`${url}${item}`)
-          const newData = await response.json()
-          if (newData.drinks) {
-            const {
-              strDrink: name,
-              strDrinkThumb: image
-            } = newData.drinks[0]
-            const newCocktail = {
-              name, image
+            const response = await fetch(`${url}${item}`)
+            const newData = await response.json()
+            if (newData.drinks) {
+                const {
+                    strDrink: name,
+                    strDrinkThumb: image
+                } = newData.drinks[0]
+                const newCocktail = {
+                    name, image
+                }
+                setCocktailnew(newCocktail)
+                setLoading(false)
             }
-            setCocktail(newCocktail)
-            
-          }
-          else {
-            setCocktail(null)
-          }
+            else {
+                setCocktailnew(null)
+                setLoading(false)
+            }
         }
         catch (error) {
-          console.log(error);
+            console.log(error);
         }
-      }, [item])
-    
-      useEffect(() => {
+    }, [item])
+
+    useEffect(() => {
         fetchData()
-      }, [item, fetchData])
-      const {name,image} = cocktail
-      
-  return (
-    <div className='d-flex justify-content-center'>
+    }, [item, fetchData])
+
+    if (loading) {
+        return (
+            <><Loading /></>
+        )
+    }
+    return (
+        <div className='d-flex justify-content-center'>
             <div className="col-md-6  mb-3" >
                 <div className="row">
                     <div className="col-4">
-                        <img src={image} className="img-fluid rounded-start" alt="..." />
+                        <img src={cocktailnew.image} className="img-fluid rounded-start" alt="..." />
                     </div>
                     <div className="col-5">
                         <div className="card-body">
-                            <h5 className="card-title">{name}</h5>
-                            <p className="card-text">Price : {}</p>
-                            <button className="text-muted" >remove</button>
+                            <h5 className="card-title">{cocktailnew.name}</h5>
+                            <p className="card-text">Price : { }</p>
+                            <button className="text-muted">remove</button>
                         </div>
                     </div>
                     <div className='col-3 '>
                         <div className='card-body '>
                             <span className='d-flex justify-content-center'>
-                                <button ><IoIosArrowUp/></button>
+                                <button ><IoIosArrowUp /></button>
                             </span>
-                            <p className='d-flex justify-content-center'>{}</p>
+                            <p className='d-flex justify-content-center'>{ }</p>
                             <span className='d-flex justify-content-center'>
-                                <button ><IoIosArrowDown/></button> 
+                                <button ><IoIosArrowDown /></button>
                             </span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-  )
+    )
 }
 
 export default Cartitem
